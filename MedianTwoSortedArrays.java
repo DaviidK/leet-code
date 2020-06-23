78f875beb28d12d186da0bbd421a8799dbaa49cf
David <49457408+DaviidK@users.noreply.github.com>
2020-06-22 18:14:49 -0700

    Added header comments

--- MedianTwoSortedArrays.java
+++ MedianTwoSortedArrays.java
@@ -1,3 +1,24 @@
+/*
+Problem sourced from: leetcode.com
+
+There are two sorted arrays nums1 and nums2 of size m and n respectively.
+
+Find the median of the two sorted arrays. The overall run time complexity 
+should be O(log (m+n)).
+
+You may assume nums1 and nums2 cannot be both empty.
+
+Example 1:
+nums1 = [1, 3]
+nums2 = [2]
+The median is 2.0
+
+Example 2:
+nums1 = [1, 2]
+nums2 = [3, 4]
+The median is (2 + 3)/2 = 2.5
+*/
+
 import java.util.Arrays;
 
 public class MedianTwoSortedArrays {
@@ -122,7 +143,7 @@ public class MedianTwoSortedArrays {
 
 /*
 
-111111
+111111  
 
 111999
 
