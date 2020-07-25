35688d674dc797fde69740024be75769ab4920e6
David Kang <49457408+DaviidK@users.noreply.github.com>
2020-07-24 18:34:29 -0700

    Started SingleNumberIII

---
+++ SingleNumber.cpp
@@ -0,0 +1,40 @@
+/*
+Problem sourced from: leetcode.com
+
+Given an array of numbers nums, in which exactly two elements appear only once 
+and all the other elements appear exactly twice. Find the two elements that 
+appear only once.
+
+Example:
+
+Input:  [1,2,1,3,2,5]
+Output: [3,5]
+
+Notes:
+-The order of the result is not important. So in the above example, [5, 3] is 
+also correct.
+  
+-Your algorithm should run in linear runtime complexity. Could you implement 
+it using only constant space complexity?
+*/
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+// Brute force solution, iterate through all numbers, and check if each number
+// appears more than once. If not, add to result
+// O(n^2) time complexity, O(n) space complexity
+vector<int> singleNumber(vector<int>& nums) {
+  vector<int> result;
+
+  for (int number : nums) {
+    if ()
+  }
+
+}
+
+int main() {
+  return 0;
+}
