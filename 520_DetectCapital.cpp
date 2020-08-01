fcab0b34ef3f2111733421e75a102cf6ebd1304e
David Kang <49457408+DaviidK@users.noreply.github.com>
2020-08-01 12:30:25 -0700

    Started DetectCapital

---
+++ 520_DetectCapital.cpp
@@ -0,0 +1,47 @@
+/*
+Problem sourced from: leetcode.com
+
+Given a word, you need to judge whether the usage of capitals in it is right or
+not.
+
+We define the usage of capitals in a word to be right when one of the following 
+cases holds:
+
+    All letters in this word are capitals, like "USA".
+    All letters in this word are not capitals, like "leetcode".
+    Only the first letter in this word is capital, like "Google".
+
+Otherwise, we define that this word doesn't use capitals in a right way.
+
+Example 1:
+Input: "USA"
+Output: True
+
+Example 2:
+Input: "FlaG"
+Output: False
+
+Note: The input will be a non-empty word consisting of uppercase and lowercase 
+latin letters.
+*/
+
+#include <iostream>     // For printing to console 
+#include <string>       // For strings
+
+using namespace std;
+
+bool detectCapitalUseBruteForce(string word) {
+  // If the string has only 1 letter, return true
+  if (word.length() == 1) {
+    return true;
+  }
+
+  // The first case will only occur if all letters are capitalized
+  if (isupper(word[0]) && )
+
+
+}
+
+int main() {
+
+}
