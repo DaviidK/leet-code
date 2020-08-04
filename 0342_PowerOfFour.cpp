20b212c9793255cca41fce0488d4a4019dc1f3ca
DaviidK <dk98226@gmail.com>
2020-08-04 11:44:25 -0700

    Began PowerOfFour

---
+++ 0342_PowerOfFour.cpp
@@ -0,0 +1,28 @@
+/*
+Problem sourced from: leetcode.com
+
+Given an integer (signed 32 bits), write a function to check whether it is a 
+power of 4.
+
+Example 1:
+Input: 16
+Output: true
+
+Example 2:
+Input: 5
+Output: false
+
+Follow up: Could you solve it without loops/recursion?
+*/
+
+#include <iostream>     // For printing to console
+
+using namespace std;
+
+bool isPowerOfFour(int num) {
+    return true;
+}
+
+int main() {
+    return 1;
+}
