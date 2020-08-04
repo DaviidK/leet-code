d2d40967f043e73e9f077823ff27c1828fe54f20
David <49457408+DaviidK@users.noreply.github.com>
2020-08-04 15:36:09 -0700

    Finished PowerOfFour

--- 0342_PowerOfFour.cpp
+++ 0342_PowerOfFour.cpp
@@ -19,10 +19,39 @@ Follow up: Could you solve it without loops/recursion?
 
 using namespace std;
 
+// O(n) solution using a loop. O(1) solution can be achieved via bit manipulation
 bool isPowerOfFour(int num) {
+    // Check edge case for 0, which is not a power of four
+    if (num == 0) {
+        return false;
+    }
+
+    // Loop through the number, dividing by four everytime it is possible to do
+    // so without a remainder. Stop when number is 1, indicating that 4 has been
+    // reached
+    while(num != 1){
+        // If the number is divisible by four, do so
+        if (num % 4 == 0){
+            num = num / 4;
+        } 
+        // If the number is not divisible by four, return false
+        else{
+            return false;
+        }
+    }
+
+    // Retrun true if the number was able to be broken down completely as a factor
+    // of four
     return true;
 }
 
 int main() {
-    return 1;
+    // cout << "Testing isPowerOfFour(16)" << endl;
+    // cout << (isPowerOfFour(16) == true ? "true" : "false") << endl;
+    cout << "isPowerOfFour(0) = " << (isPowerOfFour(0) == true ? "true" : "false") << endl;
+    cout << "isPowerOfFour(1) = " << (isPowerOfFour(1) == true ? "true" : "false") << endl;
+    cout << "isPowerOfFour(4) = " << (isPowerOfFour(4) == true ? "true" : "false") << endl;
+    cout << "isPowerOfFour(16) = " << (isPowerOfFour(16) == true ? "true" : "false") << endl;
+    cout << "isPowerOfFour(15) = " << (isPowerOfFour(15) == true ? "true" : "false") << endl;
+    cout << "isPowerOfFour(65536) = " << (isPowerOfFour(65536) == true ? "true" : "false") << endl;
 }
