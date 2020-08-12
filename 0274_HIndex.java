cb4cfee530eb47e3c4d4cb74a527236ec4533ea0
DaviidK <dk98226@gmail.com>
2020-08-11 23:13:51 -0700

    Started HIndex

---
+++ 0274_HIndex.java
@@ -0,0 +1,32 @@
+/*
+Problem sourced from: leetcode.com
+
+Given an array of citations (each citation is a non-negative integer) of a 
+researcher, write a function to compute the researcher's h-index.
+
+According to the definition of h-index on Wikipedia: "A scientist has index h if
+h of his/her N papers have at least h citations each, and the other N − h papers 
+have no more than h citations each."
+
+Example:
+Input: citations = [3,0,6,1,5]
+Output: 3 
+Explanation: [3,0,6,1,5] means the researcher has 5 papers in total and each of 
+             them had received 3, 0, 6, 1, 5 citations respectively. 
+             Since the researcher has 3 papers with at least 3 citations each 
+             and the remaining two with no more than 3 citations each, her 
+             h-index is 3.
+
+Note: If there are several possible values for h, the maximum one is taken as 
+the h-index.
+
+*/
+
+import java.util.*;     // For map of TrieNode children
+
+// Java doesn't allow underscores in file names, so it's necessary to rename the 
+// file before compiling. I chose to keep the underscore for easy organization
+// within my git repo, given that these files are often never compiled after being 
+// committed
+public class HIndex {
+}
