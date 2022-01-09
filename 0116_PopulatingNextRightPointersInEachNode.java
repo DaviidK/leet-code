a90c31b9d40c748ee2cd3a231ceebf015c5cd95d
David Kang <49457408+DaviidK@users.noreply.github.com>
2022-01-08 19:26:20 -0800

    Updated comment for #116 - Populating Next Right Pointers in Each Node

--- 0116_PopulatingNextRightPointersInEachNode.java
+++ 0116_PopulatingNextRightPointersInEachNode.java
@@ -1,4 +1,5 @@
 /*
+Problem sourced from: leetcode.com
 You are given a perfect binary tree where all leaves are on the same level, and every parent has two
 children. The binary tree has the following definition:
 
